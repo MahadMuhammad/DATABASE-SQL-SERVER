9b4ea05fa2c8c0964c74234f30e123f547d2c459
Muhammad Mahad <mahadtxt@gmail.com>
2023-02-19 17:30:05 +0500

    ASS:02 :Added DATA sent by TA

--- DB-Ass-2/q2.sql
+++ DB-Ass-2/q2.sql
@@ -6,43 +6,54 @@
 -- 2.	Insert 5 or more tuples in each of the above tables.
 
 -- TABLE 1
-INSERT INTO Event VALUES (1, 'Future Fest','Expo Centre, Lahore','conferences', '2023-01-01', '8:00 AM', '2023-01-03', '5:00 PM');
-INSERT INTO Event VALUES (2, 'SOFTEC', 'FAST-NUCES, Lahore','conferences', '2023-05-01', '8:00 AM', '2023-05-03', '5:00 PM');
-INSERT INTO Event VALUES (3, 'Coding Guru', 'LUMS', 'workshop', '2023-03-01', '8:00 AM', '2023-03-03', '5:00 PM');
-INSERT INTO Event VALUES (4, 'NASCON', 'FAST-NUCES, Islamabad', 'conferences', '2023-04-01', '8:00 AM', '2023-04-03', '5:00 PM');
-INSERT INTO Event VALUES (5, 'Takra', 'UCP, Lahore', 'seminar', '2023-02-01', '8:00 AM', '2023-02-03', '5:00 PM');
+INSERT INTO Event VALUES 
+(1,'ABC','lahore','Seminar','2022-02-22','02:00:00','2022-02-22','06:00:00'),
+(2,'DEF','lahore','Seminar','2022-01-25','02:00:00','2022-01-25','06:00:00'),
+(3,'GHI','lahore','Seminar','2022-12-22','02:00:00','2022-12-22','06:00:00'),
+(4,'JKL','lahore','Conference','2022-02-22','02:00:00','2022-02-22','06:00:00'),
+(5,'MNO','lahore','Workshop','2022-12-22','02:00:00','2022-12-22','06:00:00'
+);
 
 SELECT * FROM Event;
 --DELETE FROM Event WHERE EID = 2;
 
 -- TABLE 2
-INSERT INTO Organization VALUES (1, 'Educative', 'Gulberg III, Lahore, Punjab');
-INSERT INTO Organization VALUES (2, 'NETSOL', 'Main Ghazi Rd, Lahore');
-INSERT INTO Organization VALUES (3, 'Folio3', 'Gulberg 2, Lahore, Punjab');
-INSERT INTO Organization VALUES (4, 'Ejad-Labs', 'Johar Town, Lahore, Punjab');
-INSERT INTO Organization VALUES (5,'Mindstorm Studios','DHA Phase 2, Lahore, Punjab');
+INSERT INTO Organization VALUES 
+(1,'SOFTEC','FAST,lahore'),
+(2,'FAST','Faisal Town,lahore'),
+(3,'Techlogix','FAST,lahore'),
+(4,'DEF','FAST,lahore'),
+(5,'XYZ','FAST,lahore');
 SELECT * FROM Organization;
 
 -- TABLE 3
-INSERT INTO Participants VALUES (1,'Muhammad Mahad','2000-01-01','M',1);
-INSERT INTO Participants VALUES (2,'Muhammad Ali Butt','2001-02-02','M',2);
-INSERT INTO Participants VALUES (3,'Zaki Haider','2002-03-03','M',3);
-INSERT INTO Participants VALUES (4,'Syeda Ayesha','2003-04-04','F',4);
-INSERT INTO Participants VALUES (5,'Zainab Rehman','2004-05-05','F',5);
+INSERT INTO Participants VALUES 
+(1,'Hashim','2002-08-10','M',1),
+(2,'Hugo','2002-08-10','M',1),
+(3,'Ali','2002-08-10','M',1),
+(4,'Asad','2002-08-10','M',1),
+(5,'Sania','2002-08-10','F',NULL),
+(6,'Hania','2002-08-10','F',NULL);
 SELECT * FROM Participants;
 
 -- TABLE 4
-INSERT INTO Sponsors VALUES (1,5,50000);
-INSERT INTO Sponsors VALUES (2,4,75000);
-INSERT INTO Sponsors VALUES (3,3,25000);
-INSERT INTO Sponsors VALUES (4,2,110000);
-INSERT INTO Sponsors VALUES (5,1,250000);
+INSERT INTO Sponsors VALUES 
+(1,1,5000),
+(2,1,1000),
+(3,3,3000),
+(4,5,4000),
+(5,3,7000),
+(2,3,9000);
 SELECT * FROM Sponsors;
 
 -- TABLE 5
-INSERT INTO Attended VALUES (5,1);
-INSERT INTO Attended VALUES (4,2);
-INSERT INTO Attended VALUES (3,3);
-INSERT INTO Attended VALUES (2,4);
-INSERT INTO Attended VALUES (1,5);
+INSERT INTO Attended VALUES 
+(1,1),
+(1,4),
+(1,2),
+(1,3),
+(1,5),
+(3,4),
+(4,1),
+(5,1);
 SELECT * FROM Attended;
